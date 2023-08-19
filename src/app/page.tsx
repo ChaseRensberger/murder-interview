cc1e2851eb95cf686d01d9f9553ccab5872eb489
ChaseRensberger <chasehrensberger@gmail.com>
2023-08-19 00:58:38 -0400

    automate responses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 import Image from "next/image";
+import { useChat } from "ai/react";
 export default function Home() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/question",
+  });
   const photoWidth = 300;
   return (
     <main className="flex min-h-screen flex-col items-center p-12 gap-12 bg-orange-200 px-96">
@@ -19,33 +23,40 @@ export default function Home() {
         className="ring-8 ring-black"
       />
       <p>
-        <span className="font-bold">Yasper: </span>No, I don't think I know the
-        answer to thaNo, I don't think I know the answer to thatNo, I don't
-        think I know the answer to thatNo, I don't think I know the answer to
-        thatNo, I don't think I know the answer to thatNo, I don't think I know
-        the answer to thatt
+        {messages.map((m, index) => (
+          <div key={m.id}>
+            <span className="font-bold">
+              {index % 2 == 0 ? "Detective Johnson" : "Yasper"}
+            </span>
+            : {m.content}
+          </div>
+        ))}
       </p>
-      <div className="flex flex-col items-center gap-4">
+      <form
+        className="flex flex-col items-center gap-4"
+        onSubmit={handleSubmit}
+      >
         <label
-          htmlFor="comment"
+          htmlFor="question"
           className="block text-md font-medium leading-6 text-gray-900"
         >
-          Ask your question:
+          Ask your questions:
         </label>
         <textarea
           rows={4}
-          name="comment"
-          id="comment"
+          name="question"
+          id="question"
           className="block w-96 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          defaultValue={""}
+          value={input}
+          onChange={handleInputChange}
         />
         <button
-          type="button"
+          type="submit"
           className="rounded bg-black px-8 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Ask
         </button>
-      </div>
+      </form>
     </main>
   );
 }
